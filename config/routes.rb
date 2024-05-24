8739d2634d237fe64b8eed2ce2301f43d3ad85b9
akadusei <attakusiadusei@gmail.com>
2024-05-24 14:18:08 +0000

    Add user registrations

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
   get "about", to: "about#index"
   # resource :about, only: [:index]
 
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
   # Defines the root path route ("/")
   root "main#index"
 end

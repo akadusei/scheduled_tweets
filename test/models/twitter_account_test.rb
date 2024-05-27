1805bbedc81b781fda936ff4f1d69f3e52ac06ff
akadusei <attakusiadusei@gmail.com>
2024-05-27 18:24:13 +0000

    Set up unit test

--- test/models/twitter_account_test.rb
+++ test/models/twitter_account_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class TwitterAccountTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "requires username" do
+    user = User.new(email: "user@example.org", password: "a1b2c3")
+    assert user.valid?
+
+    twitter_account = TwitterAccount.new(user: user)
+    assert_not twitter_account.valid?
+
+    assert_includes twitter_account.errors.messages[:username], "can't be blank"
+  end
 end
